88f5092ba1572d472930989b08baf14dd4e6c2c3
JamesVictor-o <victorjames40v@gmail.com>
2025-09-13 04:25:49 +0100

    fix: fixed test error for yieldvault contract

--- contracts/src/AaveStrategy.sol
+++ contracts/src/AaveStrategy.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.20;
 
 import "./IStrategy.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {DataTypes} from "@aave/core-v3/contracts/protocol/libraries/types/DataTypes.sol"; 
+
+
 
 interface IAavePool {
     function supply(
@@ -17,7 +20,7 @@ interface IAavePool {
         uint256 amount,
         address to
     ) external returns (uint256);
-
+     function getReserveData(address asset) external view returns (DataTypes.ReserveData memory);
     function getUserAccountData(
         address user
     )
@@ -33,20 +36,43 @@ interface IAavePool {
         );
 }
 
+interface IAToken {
+    function getScaledUserBalance(address user) external view returns (uint256);
+    function scaledBalanceToTotalUnderlyingBalance(uint256 scaledBalance) external view returns (uint256);
+    function balanceOf(address user) external view returns (uint256);
+    function redeem(address user, uint256 amount, address to) external;  // For emergencyWithdraw
+}
+
 contract AaveStrategy is IStrategy {
     IAavePool public aavePool;
     IERC20 public asset;
     address public vault;
+    address public immutable owner;
+    IAToken public aToken;  // Reference to the aToken for this asset
 
     constructor(address _aavePool, address _asset, address _vault) {
         aavePool = IAavePool(_aavePool);
         asset = IERC20(_asset);
         vault = _vault;
+        owner = msg.sender;
+        DataTypes.ReserveData memory reserveData = aavePool.getReserveData(_asset);
+        require(reserveData.aTokenAddress != address(0), "Reserve not initialized on Aave");
+        aToken = IAToken(reserveData.aTokenAddress);
     }
 
     modifier onlyVault() {
         require(msg.sender == vault, "Not vault");
         _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    function setVault(address _vault) external onlyOwner {
+        require(_vault != address(0), "Invalid vault address");
+        vault = _vault;
     }
 
     function invest(uint256 amount) external override onlyVault {
@@ -59,14 +85,24 @@ contract AaveStrategy is IStrategy {
     }
 
     function totalAssets() external view override returns (uint256) {
-        // For simplicity, just return asset balance. For full integration, query aToken balance.
-        return asset.balanceOf(address(this));
+        // Get total supplied (aToken balance converted to underlying)
+        uint256 scaledBalance = aToken.getScaledUserBalance(address(this));
+        uint256 supplied = aToken.scaledBalanceToTotalUnderlyingBalance(scaledBalance);
+        // Plus any idle underlying tokens in the strategy
+        uint256 idle = asset.balanceOf(address(this));
+        return supplied + idle;
     }
 
     function emergencyWithdraw() external override onlyVault {
-        uint256 balance = asset.balanceOf(address(this));
-        if (balance > 0) {
-            aavePool.withdraw(address(asset), balance, vault);
+        // Withdraw any idle balance first
+        uint256 idle = asset.balanceOf(address(this));
+        if (idle > 0) {
+            aavePool.withdraw(address(asset), idle, vault);
+        }
+        // Then full redeem from aToken
+        uint256 aTokenBalance = aToken.balanceOf(address(this));
+        if (aTokenBalance > 0) {
+            aToken.redeem(address(this), aTokenBalance, vault);
         }
     }
 }
